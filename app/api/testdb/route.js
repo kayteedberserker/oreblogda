2d6113472db7c48009ff2607b877a145950742dd
Ukabir <122126648+Ukabir@users.noreply.github.com>
2025-10-25 01:21:00 +0100

    Update route.js

--- app/api/testdb/route.js
+++ app/api/testdb/route.js
@@ -1,14 +1,14 @@
-import { NextResponse } from "next/server";
-import { connectDB } from "@/app/lib/mongodb";
+//import { NextResponse } from "next/server";
+// import { connectDB } from "@/app/lib/mongodb";
 
-export async function GET() {
-  try {
-    await connectDB();
-    return NextResponse.json({ message: "✅ MongoDB connected successfully!" });
-  } catch (error) {
-    return NextResponse.json(
-      { message: "❌ Database connection failed", error: error.message },
-      { status: 500 }
-    );
-  }
-}
+// export async function GET() {
+ // try {
+   // await connectDB();
+  //  return NextResponse.json({ message: "✅ MongoDB connected successfully!" });
+ // } catch (error) {
+  //  return NextResponse.json(
+  //    { message: "❌ Database connection failed", error: error.message },
+  //    { status: 500 }
+ //   );
+//  }
+//}
