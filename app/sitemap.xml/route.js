9cffb7d53db3eaf097209f450dc24b614dcb0ef1
Ukabir <122126648+Ukabir@users.noreply.github.com>
2025-10-25 01:34:20 +0100

    Update route.js

--- app/sitemap.xml/route.js
+++ app/sitemap.xml/route.js
@@ -1,5 +1,24 @@
-import { connectDB } from "@/app/lib/mongodb";
+// import { connectDB } from "@/app/lib/mongodb";
 import Post from "@/app/models/PostModel";
+
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+function connectDB() {
+  if (isConnected) return;
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    isConnected = true;
+    console.log("✅ MongoDB connected");
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error);
+  }
+}
+
+
+
 
 export async function GET() {
   await connectDB();
