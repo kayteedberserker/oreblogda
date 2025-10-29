fce18ea0b44969b39378ed293850a7401b8f1e8b
Ukabir <ukabir902@gmail.com>
2025-10-29 15:07:29 +0100

    still

--- next.config.js
+++ next.config.js
@@ -5,12 +5,15 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"], // allow Cloudinary images
   },
+  reactStrictMode: true,
+};
+
+export default withPWA({
+  ...nextConfig,
   pwa: {
     dest: "public",
     register: true,
     skipWaiting: true,
     disable: process.env.NODE_ENV === "development",
   },
-};
-
-export default withPWA(nextConfig);
+});
