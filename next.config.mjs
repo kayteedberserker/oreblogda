754eeb52e2f4d1781068f2a91e07a104bfe333a3
Ukabir <ukabir902@gmail.com>
2025-10-29 14:38:38 +0100

    nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,17 @@
-/** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa")({
-  dest: "public", // service worker and manifest go here
-  disable: process.env.NODE_ENV === "development", // disables PWA in dev
-});
+// next.config.mjs
+import withPWA from "next-pwa";
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com"], // allow Cloudinary images
+    domains: ["res.cloudinary.com"],
   },
-  reactStrictMode: true,
 };
 
-module.exports = withPWA(nextConfig);
+export default withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+  ...nextConfig,
+});
