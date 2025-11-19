0d9d390590dd93ae14bd4d7b2f67e8119cf1fd84
Ukabir <122126648+Ukabir@users.noreply.github.com>
2025-11-19 13:20:21 +0100

    Restrict ads to only the homepage

--- app/components/Adsense.js
+++ app/components/Adsense.js
@@ -6,22 +6,8 @@ import { usePathname } from "next/navigation";
 export default function Adsense() {
   const pathname = usePathname();
 
-  // Pages where you don't want ads
-  const noAdsPages = [
-    "/auth/login",
-    "/auth/signup",
-    "/not-found",
-    "/authordiary",
-    "/contact",
-    "/about",
-    "/terms",
-    "/privacy",
-    "/authordiary/profile"
-  ];
-
-  const showAds = !noAdsPages.includes(pathname);
-
-  if (!showAds) return null; // Don't render anything on excluded pages
+  // Only show ads on the homepage
+  if (pathname !== "/") return null;
 
   return (
     <Script
