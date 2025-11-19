4d10b55fb749461433ce61179f477c6a863004b6
Ukabir <122126648+Ukabir@users.noreply.github.com>
2025-11-19 15:00:57 +0100

    Refactor CategoryPage for cleaner parameter handling

--- app/categories/[id]/page.jsx
+++ app/categories/[id]/page.jsx
@@ -3,20 +3,18 @@ import ClientCategoryPage from "./ClientCategoryPage";
 const limit = 5;
 
 export default async function CategoryPage({ params }) {
-  console.log(" Paramus:" , {params}) 
-  const { id } = params;
-console.log(id) 
+  // destructure directly
+  const { id } = params; 
+  console.log("id:", id); // should log 'polls'
+  
   const category = id
     ? id.includes("-")
-      ? id
-          .split("-")
-          .map((w) => w.charAt(0).toUpperCase() + w.slice(1))
-          .join("/")
+      ? id.split("-").map((w) => w.charAt(0).toUpperCase() + w.slice(1)).join("/")
       : id.charAt(0).toUpperCase() + id.slice(1).toLowerCase()
     : "";
-console.log(category) 
+
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/posts?category=${category}&page=1&limit=${limit}`
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/posts?category=${category}&page=1&limit=5`
   );
   const data = await res.json();
   const initialPosts = data.posts || [];
