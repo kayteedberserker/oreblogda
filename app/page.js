2bfeed6b5e91d1cac06e2382cb21b05b6e627760
Ukabir <122126648+Ukabir@users.noreply.github.com>
2025-11-26 11:52:48 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import { ToastContainer } from "react-toastify";
 import Navbar from "@/app/components/Navbar";
 
 export default async function HomePage() {
-  const limit = 10;
+  const limit = 5;
 
   // Fetch posts on the server BEFORE rendering
   const res = await fetch(
