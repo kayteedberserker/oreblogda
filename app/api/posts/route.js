07cebd34a07311a09e319a7768bd5cbe18d3fa9f
Ukabir <122126648+Ukabir@users.noreply.github.com>
2025-11-30 09:41:27 +0100

    Increase default limit for posts from 5 to 20

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -16,7 +16,7 @@ export async function GET(req) {
   try {
     const { searchParams } = new URL(req.url);
     const page = parseInt(searchParams.get("page")) || 1;
-    const limit = parseInt(searchParams.get("limit")) || 5;
+    const limit = parseInt(searchParams.get("limit")) || 20;
     const author = searchParams.get("author");
     const category = searchParams.get("category");
     const skip = (page - 1) * limit;
