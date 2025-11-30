9e83b128790cf3d88eef163085ea18b9d1662266
Ukabir <122126648+Ukabir@users.noreply.github.com>
2025-11-30 08:22:36 +0100

    Add 'Review' category and adjust nav styles

--- app/components/CategoryNav.jsx
+++ app/components/CategoryNav.jsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-const categories = ["News", "Memes", "Videos/Edits", "Polls"];
+const categories = ["News", "Memes", "Videos/Edits", "Polls", "Review"];
 
 export default function CategoryNav() {
   const pathname = usePathname(); // current URL
@@ -11,14 +11,14 @@ export default function CategoryNav() {
 
   if (hideNavbarRoutes.includes(pathname)) return null;
   return (
-    <nav className="flex space-x-1 sm:space-x-3 justify-center overflow-x-auto py-2 px-3 sm:px-4 bg-gray-100 dark:bg-gray-800 rounded-md shadow-sm">
+    <nav className="flex space-x-1 sm:space-x-3 justify-center overflow-x-auto py-2 px-2 sm:px-4 bg-gray-100 dark:bg-gray-800 rounded-md shadow-sm">
       {categories.map((cat) => {
         const isActive = pathname.includes(cat.toLowerCase().replace("/", "-"));
         return (
           <Link
             key={cat}
             href={`/categories/${cat.toLowerCase().replace("/", "-")}`}
-            className={`inline-flex items-center px-3 py-1 rounded-md font-medium whitespace-nowrap ${isActive
+            className={`inline-flex items-center px-1 text-sm py-1 rounded-md font-medium whitespace-nowrap ${isActive
                 ? "bg-blue-600 text-white"
                 : "bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200 hover:bg-blue-500 hover:text-white transition"
               }`}
