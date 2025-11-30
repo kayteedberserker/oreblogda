0260dac80106a7e10db011085484c5409231b32d
Ukabir <122126648+Ukabir@users.noreply.github.com>
2025-11-30 08:26:00 +0100

    Adjust padding in CategoryNav component

--- app/components/CategoryNav.jsx
+++ app/components/CategoryNav.jsx
@@ -11,14 +11,14 @@ export default function CategoryNav() {
 
   if (hideNavbarRoutes.includes(pathname)) return null;
   return (
-    <nav className="flex space-x-1 sm:space-x-3 justify-center overflow-x-auto py-2 px-2 sm:px-4 bg-gray-100 dark:bg-gray-800 rounded-md shadow-sm">
+    <nav className="flex space-x-2 sm:space-x-3 justify-center overflow-x-auto py-2 px-2 sm:px-4 bg-gray-100 dark:bg-gray-800 rounded-md shadow-sm">
       {categories.map((cat) => {
         const isActive = pathname.includes(cat.toLowerCase().replace("/", "-"));
         return (
           <Link
             key={cat}
             href={`/categories/${cat.toLowerCase().replace("/", "-")}`}
-            className={`inline-flex items-center px-1 text-sm py-1 rounded-md font-medium whitespace-nowrap ${isActive
+            className={`inline-flex items-center px-2 text-sm py-1 rounded-md font-medium whitespace-nowrap ${isActive
                 ? "bg-blue-600 text-white"
                 : "bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200 hover:bg-blue-500 hover:text-white transition"
               }`}
