4cbccb0745f4d113b826b191f11974b70611aa41
Ukabir <122126648+Ukabir@users.noreply.github.com>
2025-12-10 19:16:39 +0100

    Update FooterAds.js

--- app/components/FooterAds.js
+++ app/components/FooterAds.js
@@ -1,23 +1,29 @@
 "use client";
 import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 
 export default function FooterAds() {
+  const pathname = usePathname();
+
   useEffect(() => {
     try {
       if (typeof window !== "undefined") {
-        (window.adsbygoogle = window.adsbygoogle || []).push({});
+        window.adsbygoogle = window.adsbygoogle || [];
+        window.adsbygoogle.push({});
       }
     } catch (e) {
       console.error("AdSense error:", e);
     }
-  }, []);
+  }, [pathname]); // run on route change
 
   return (
-    <div className=" flex mb-2.5 justify-center w-full">
-        <ins class="adsbygoogle"
-     style={{display: "inline-block", width: "300px", height: "200px"}}
-     data-ad-client="ca-pub-8021671365048667"
-     data-ad-slot="9897041432"></ins>
+    <div className="flex mb-2.5 justify-center w-full">
+      <ins
+        className="adsbygoogle"
+        style={{ display: "inline-block", width: "300px", height: "200px" }}
+        data-ad-client="ca-pub-8021671365048667"
+        data-ad-slot="9897041432"
+      ></ins>
     </div>
   );
 }
