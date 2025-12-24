8880174287d0dce44147b149ebdb9b93e22b8a64
Ukabir <ukabir902@gmail.com>
2025-12-24 23:30:00 +0100

    updated nextconfig to redirect to download

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"], // allow Cloudinary images
   },
+  async redirects() {
+    return [
+      {
+        source: '/download',
+        destination: 'https://expo.dev/artifacts/eas/t81hLZrqffFmwSjYZRvHEo.apk',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
