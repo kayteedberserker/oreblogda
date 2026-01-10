45f28b8b4834bbb4ca2f217523ffb29920150c37
Ukabir <ukabir902@gmail.com>
2026-01-10 23:19:35 +0100

    twitter image hopeful fix

--- app/post/[id]/page.jsx
+++ app/post/[id]/page.jsx
@@ -16,7 +16,16 @@ export async function generateMetadata({ params }) {
 
   const description = post.message?.slice(0, 150) || "Read this post on Oreblogda";
   const postUrl = `https://oreblogda.com/post/${post.slug || post._id}`;
-  const postImage = post.mediaUrl ? post.mediaUrl.includes("res.cloudinary.com") ? post.mediaUrl : "https://oreblogda.com/ogimage.png" : "https://oreblogda.com/ogimage.png";
+  let postImage = "https://oreblogda.com/ogimage.png";
+  if (post.mediaUrl) {
+	if (post.mediaUrl.includes("res.cloudinary.com")) {
+		postImage = post.mediaUrl;
+	}else{
+		postImage = "https://oreblogda.com/ogimage.png";
+	}
+  }else {
+	postImage = "https://oreblogda.com/ogimage.png";
+  }
 
   return {
     authors: [
