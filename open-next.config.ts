2f2345c562738023308fb2adff3ab8186e5d9eac
Ukabir <ukabir902@gmail.com>
2026-01-11 08:24:08 +0100

    one more

---
+++ open-next.config.ts
@@ -0,0 +1,5 @@
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+
+export default defineCloudflareConfig({
+  // This is the default configuration for Next.js on Cloudflare Workers
+});
