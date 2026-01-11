2c1b05dc0c0af0231c2e8face91c6b52bc57a78c
Ukabir <ukabir902@gmail.com>
2026-01-11 09:04:58 +0100

    new

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-
+export const runtime = 'experimental-edge'; // <--- ADD THIS LINE
 export function middleware(req) {
   const { pathname } = req.nextUrl;
 
