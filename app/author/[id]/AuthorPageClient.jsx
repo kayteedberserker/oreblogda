7469d01cf589e91ba3dcf238728c8fc3600ed24b
kayteedberserker <kayteedberserker@gmail.com>
2026-01-14 17:42:41 +0100

    Update layout of sponsored transmission ad section

--- app/author/[id]/AuthorPageClient.jsx
+++ app/author/[id]/AuthorPageClient.jsx
@@ -161,7 +161,7 @@ export default function AuthorPageClient({ author, initialPosts = [], total: ini
             >
               <PostCard post={post} isFeed />
               {(index + 1) % 2 === 0 && (
-                     <div className="my-10 w-full p-4 border border-dashed border-gray-200 dark:border-gray-800 rounded-3xl flex flex-col align-center justify-center bg-gray-50/50 dark:bg-white/5">
+                     <div className="my-10 w-full p-4 px-2 border border-dashed border-gray-200 dark:border-gray-800 rounded-3xl flex flex-col items-center gap-1 justify-center bg-gray-50/50 dark:bg-white/5">
                       <span className="text-[9px] font-bold text-gray-400 uppercase tracking-widest italic">Sponsored Transmission</span>
                        <FeedAd /> 
                   </div>
