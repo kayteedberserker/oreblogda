d51273029896298f9409e771519b7a6a06cfc7d9
kayteedberserker <kayteedberserker@gmail.com>
2026-01-16 12:09:09 +0100

    Update page.js

--- app/authordiary/dashboard/page.js
+++ app/authordiary/dashboard/page.js
@@ -95,7 +95,7 @@ export default function FullAdminDashboard() {
             }
         };
         fetchUser();
-        fetchCurrentVersion();
+        
     }, [router]);
 
     useEffect(() => {
